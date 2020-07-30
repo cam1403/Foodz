6db0c9e67098eaf90b4437e665c0c22845f780ee
Chris Campos <camposchris18@gmail.com>
2020-07-30 11:31:11 -0400

    API Call

---
+++ src/components/apiSearchModel.js
@@ -0,0 +1,9 @@
+const Axios = require('axios');
+
+const searchIng = (search) => {
+  Axios.get(
+    `https://api.edamam.com/api/food-database/v2/parser?app_id=54bc6595&app_key=b6f3657e19180bce0e858384a4ea8858&ingr=${search}`
+  ).then((res) => {
+    console.log(res.data);
+  });
+};
