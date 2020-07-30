78e9bf99d80bd272da516a00adcea06333a40244
Chris Campos <camposchris18@gmail.com>
2020-07-30 10:49:36 -0400

    Initial App Set up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+
+const App = (props) => {
+  return (
+    <>
+      <p>THIS IS AN APP!</p>
+    </>
+  );
+};
+
+export default hot(App);
